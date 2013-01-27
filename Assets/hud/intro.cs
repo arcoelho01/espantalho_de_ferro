94dd15bfb5c30d6c3e611a254a7ebb76dc6ab6b2
Gustavo Roman Marangon <gromanmarangon@gmail.com>
2013-01-27 15:24:14 -0200

    Final Intro+Menu DONE

---
+++ Assets/hud/intro.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class intro : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnMouseOver(){
+		if(Input.anyKey){
+			Application.LoadLevel("menuScene");
+		}
+	}
+}
