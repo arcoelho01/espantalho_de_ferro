c54263212f08ce137f6c551d7449230d9b911bf2
Rodrigo <digo.n.punk@gmail.com>
2013-01-27 12:11:35 -0200

    Heart doesnt bug under the mouse

--- Assets/Heart/Heart.cs
+++ Assets/Heart/Heart.cs
@@ -21,8 +21,10 @@ public class Heart : MonoBehaviour {
 			Vector3 mousePos = Camera.mainCamera.ScreenToWorldPoint(Input.mousePosition);
 			Vector2 moveDir= new Vector2(0,0);
 			moveDir=new Vector2(mousePos.x-transform.position.x,mousePos.z-transform.position.z).normalized;
-	
-			character.Move(new Vector3(moveDir.x,0,moveDir.y)*speed*Time.deltaTime);
+			Vector2 distance = new Vector2(mousePos.x,mousePos.z)-new Vector2(transform.position.x, transform.position.z);
+			Debug.Log ("Magnitude: "+distance.magnitude);
+			if(distance.magnitude>1)
+				character.Move(new Vector3(moveDir.x,0,moveDir.y)*speed*Time.deltaTime);
 			
 		}
 	}
